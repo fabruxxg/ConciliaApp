b06ae0f0e5df12e9578a5e084127fe52a51f17ec
fabruxxg <fabrigaoli@gmail.com>
2026-06-01 17:39:30 -0300

    Fix: Agregar get_session a database.py

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
 import os
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session # <- Revisa tener 'Session' aquí
+from models import User # <- Dejamos esto aquí para que cree la tabla de usuarios
 
-# Esto busca la dirección en Railway automáticamente
-# Si no la encuentra, fallará (lo cual está bien, porque así sabes que falta configurar)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
+
+# !!! ASEGÚRATE DE QUE ESTA FUNCIÓN ESTÉ ESCRITA EXACTAMENTE ASÍ AL FINAL !!!
+def get_session():
+    with Session(engine) as session:
+        yield session
